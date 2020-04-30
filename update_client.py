8dc0dfc824d6f1d3fa1726adeb0408fd6c4c3ed0
Marco Wiederaenders <34957150+marcwied@users.noreply.github.com>
2020-04-30 09:09:29 -0600

    update_client_V2
    -takes into account all available headers from API
    -handles null or '' values in columns and will remove from json payload
    -split engines between loyalty and non loyalty integrated brands

--- update_client.py
+++ update_client.py
@@ -1,102 +1,159 @@
-# import getpass
-import pyodbc
-import pypyodbc
-import monkeyFunctions as mnky
-import sql
-import csv
-import os
-import pandas as pd
 import requests
 import json
+import csv
 
 
 
 
 # API Work
 ############################################################################################################
-# required credentials to get auth token assigned to variables
+#required credentials to get auth token assigned to variables
 api_key = input("Enter the Client API Key: ")
 type(api_key)
 api_secret = input("Enter the Client API Secret: ")
 type(api_secret)
 
-
-def get_auth_token(key=None):                                                           # function to get API auth token
+"""
+function to get API auth token
+args: none
+returns: access token string
+"""
+def get_auth_token(key=None):
     if key is None:
         key = {'client_id': api_key, 'client_secret': api_secret}
-    dest_token = requests.get('url goes here', params=key)
+    dest_token = requests.get('https://monkeyconnect.mnkysoft.com/monkeytoken', params=key)
     dest_access_token = dest_token.json()
     type(dest_access_token)
-    return dest_access_token['access_token']                                            # returns a dictionary value for specific key 'access_token'
-#
-#
-# print(get_auth_token())                         # Test Function
-#
-#
-def put_clients(data_file,member_id):                                                    # Function to put (update) clients, requires loyaltyPrograms node
+    return dest_access_token['access_token']
+
+# Test Function
+print(get_auth_token())
+
+"""
+function to put(send) client information
+args: cleaned json file, member(from .csv)
+returns: Response Status Code, 200 = Success
+"""
+def put_clients(data_file,member_id):
     headers = {'content-type': 'application/json', 'Authorization': 'Bearer ' + str(get_auth_token())}
-    print(headers)
-    put_response = requests.put('url goes here' + str(member_id),
+    put_response = requests.put('https://monkeyconnect.mnkysoft.com/clients/' + str(member_id),
                                 data=data_file, headers=headers)
-    return put_response                                                                 # returns status code of API request
-#
-# # print(put_clients(240178))                    # Test Function
-#
-#
-def get_clients(member_id):                                                             # Function to get clients, only returns client loyalty Programs node
+    return put_response
+
+# Test Function
+# print(put_clients(240178))
+
+
+"""
+function to get(retrieve) client loyalty information
+args: member(from .csv)
+returns: client loyalty information as a list[Dictionary]
+"""
+def get_clients(member_id):
     headers = {'content-type': 'application/json', 'Authorization': 'Bearer ' + str(get_auth_token())}
-    get_response = requests.get('url goes here' + str(member_id), headers=headers)
-    return get_response.json()['loyaltyPrograms'][0]                                      # Returns dictionary {key: value} of selected columns
-#
-#print(get_clients(240178))                      # Test Function
-############################################################################################################
+    get_response = requests.get('https://monkeyconnect.mnkysoft.com/clients/' + str(member_id), headers=headers)
+    return get_response.json()['loyaltyPrograms']
 
-# CSV work
-############################################################################################################
-from csv import DictReader
-
-def setup_data(file):                                                                   # wrapper function
-
-    with open(file,'r') as f:                                                           # opens .csv of data type "file", read only, aliased as f
-        reader = DictReader(f)                                                          # creation of DictReader object (basically a cursor)
-        for row in reader:                                                              # creation of variable that iterates through each line
-            member_id = row['id']                                                       # variable assigned to Dict(Key) = 'id'
-            prelim_json = row                                                           # assigns variable to current row (dictionary type)
-            prelim_json['loyaltyPrograms'] = [get_clients(member_id)]                   # creates Dict(Key) loyaltyPrograms, assigns results of get_clients(current member_id) as value
-            del prelim_json['id']                                                       # client_id not editable in API, removes from Dict
-
-            print(json.dumps(prelim_json))                                              # prints raw data being sent in request
-
-            print(put_clients(json.dumps(prelim_json), member_id))                      # sends request and prints result code
-
-
-setup_data('temp_test.csv')                                                             # file for wrapper function
-
+#Test Function
+#print(get_clients(240178))
 
 ############################################################################################################
+
+# .csv work - script engine
+############################################################################################################
+'''
+Script Considerations: 
+
+1. .csv must include id column
+2. script engines are split between loyalty/non loyalty integrated brands - will prompt at start of program
+3. if your brand is integrated with loyalty but your not updating a loyaltyCardNumber for a client, leave the field blank in the .csv
+
+'''
+
+# prompts for loyalty status of brand
+loyalty_integration = input("Does the brand have loyalty integration? (Y/N): ").lower()
+
+# non-loyalty engine
+if loyalty_integration == 'n':
+
+    def setup_data(file):  # wrapper function
+
+        with open(file, 'r') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                member_id = row['id']
+
+                prelim_json = row
+                del prelim_json['id']
+                for key, values in list(prelim_json.items()):
+                    if values == '':
+                        del prelim_json[key]
+
+                print(json.dumps(prelim_json))
+
+                print(put_clients(json.dumps(prelim_json), member_id))
+
+
+    setup_data('temp_test.csv')
+
+#loyalty engine
+elif loyalty_integration == 'y':
+
+
+    def setup_data(file):
+        with open(file, 'r') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                prelim_json = row
+                member_id = prelim_json['id']
+
+                if prelim_json['loyaltyCardNumber'] == '':
+                    prelim_json['loyaltyPrograms'] = get_clients(member_id)
+
+                else:
+                    prelim_json['loyaltyPrograms'] = get_clients(member_id)
+                    prelim_json['loyaltyPrograms'][0]['loyaltyCardNumber'] = row['loyaltyCardNumber']
+                    del prelim_json['loyaltyCardNumber']
+
+
+                for key, values in list(prelim_json.items()):
+                    if values == '':
+                        del prelim_json[key]
+
+                del prelim_json['id']
+
+                payload = json.dumps(prelim_json)
+
+                print(f'{member_id} updated with values {payload}')
+                print(f'{put_clients(payload, member_id)}      Success!')
+
+    setup_data('temp_test.csv')
+
+
+###########################################################################################################
 
 
 # DB Connection
-
-############################################################################################################
-
+#
+# ###########################################################################################################
+#
 # dbserver = 'server goes here'
 # db = "db name here"
 # dbuser = 'username here'
 # driver = 'ODBC Driver 17 for SQL Server'
 # dbpass = 'pwgoeshere'
-
-
+#
+#
 # print(pyodbc.drivers())
-
+#
 # connection string using pypyodbc, added ssl encryption, and server certificate exemptions - still gives login timeout error
-
+#
 # conn_string = 'Driver={ODBC Driver 17 for SQL Server};Server=' + dbserver + ';Database=' + db + ';UID=' + dbuser + ';' + 'EncryptionMethod=SSL;' + 'Encrypt=yes;' + 'TrustServerCertificate=yes;' + 'hostNameInCertificate=*.database.windows.net;' + 'Connection Timeout=30;' + 'Authentication=ActiveDirectoryInteractive;'
 # cnct = pypyodbc.connect(conn_string)
-
-
+#
+#
 # connection string using pyodbc gives login timeout
-
+#
 # cnxn = pyodbc.connect("driver={ODBC Driver 17 for SQL Server};"
 #                       "Server="+dbserver+";"
 #                       "Database="+db+";"
@@ -110,10 +167,10 @@ setup_data('temp_test.csv')                                                     
 # while row:
 #     print (str(row[0]) + " " + str(row[1]))
 #     row = cursor.fetchone()
-
-
+#
+#
 # connection string using mnkyfunctions (pypyodbc) - doesnt work, login timeout
-
+#
 # connection = monkeyFunctions.DBConnections(dbserver, dbuser, dbpass).azure_connect('dev223_monkeymedia_ca_01')
 
-############################################################################################################
+###########################################################################################################
